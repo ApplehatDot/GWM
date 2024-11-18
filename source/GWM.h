c7d6c3da9e70630efd993ad1bb87f55c66451a24
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2024-11-18 17:18:55 +0100

    Create GWM.h

---
+++ source/GWM.h
@@ -0,0 +1,21 @@
+#ifndef GWM_GL_CORE_H
+#define GWM_GL_CORE_H
+
+typedef struct {
+	float WGL_X;
+	float WGL_Y;
+	float PosX;
+	float PosY;
+} WindowPosition;
+
+typedef struct {
+	float WinX;
+	float WinY;
+	char* DESC;
+	float DescX;
+	float DescY;
+} GWMConfig;
+
+void MakeWindow(GWMConfig *GWM, WindowPosition *WP);
+
+#endif // GWM_GL_CORE_H
